7051af8c2980253ccd685ce08c0b631f5e6cc15a
Kevin Deganos and Ming Wen <ming@>
2016-05-31 15:34:55 -0700

    Add stateful pipe and list update

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,26 +2,37 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent, EditTaskDetailsComponent],
+  pipes: [DonePipe],
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <task-display *ngFor="#currentTask of taskList"
+  <select (change)="onChange($event.target.value)" class="filter">
+    <option value="all">Show All</option>
+    <option value="done">Show Done</option>
+    <option value="notDone" selected="selected">Show Not Done</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
   </task-display>
   <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
   </edit-task-details>
+  <br><br><br><br><br><br>
+  <new-task (onSubmitNewTask)="createTask($event)"></new-task>
   `
 })
 export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -29,5 +40,13 @@ export class TaskListComponent {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
   }
 }
