c8fd4e6faf4557c67a28f1f5fe5cbe3531640a54
Kevin Deganos and Ming Wen <ming@>
2016-05-31 09:25:48 -0700

    Displaying data in a view

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
+}
